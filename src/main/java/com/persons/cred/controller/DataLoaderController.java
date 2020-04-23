18fefdd4060764b8c7b634f7460c8eac9d6ba40f
pnegi-centrica <preeti.negi@centrica.com>
2020-04-23 17:39:38 +0100

    RefactoringAndBusinessLogicMovedToService

--- src/main/java/com/persons/cred/controller/DataLoaderController.java
+++ src/main/java/com/persons/cred/controller/DataLoaderController.java
@@ -1,15 +1,6 @@
 package com.persons.cred.controller;
 
-import com.persons.cred.entiities.Person;
-import com.persons.cred.entiities.WebSite;
-import com.persons.cred.repositories.PersonRepository;
-import com.persons.cred.repositories.WebsiteRepository;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFRow;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import com.persons.cred.services.DataLoaderService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
@@ -18,89 +9,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Optional;
 
-@Slf4j
 @RestController
 public class DataLoaderController {
 
     @Autowired
-    private PersonRepository personRepository;
-    @Autowired
-    private WebsiteRepository websiteRepository;
+    private DataLoaderService dataLoaderService;
+
     //Add a controller to upload excel
     @GetMapping("/read")
     public String  readExcel() throws IOException {
-
         Resource resource = new ClassPathResource("Codes.xlsx");
-
         InputStream input = resource.getInputStream();
-
-        try (XSSFWorkbook workbook = new XSSFWorkbook(input)){
-            XSSFSheet spreadsheet = workbook.getSheetAt(2);
-            Iterator<Row> rowIterator = spreadsheet.iterator();
-
-            List<Person> personList = personRepository.findAll();
-
-            while (rowIterator.hasNext()) {
-                XSSFRow row = (XSSFRow) rowIterator.next();
-                Iterator<Cell> cellIterator = row.cellIterator();
-                while ( cellIterator.hasNext()) {
-                    Cell cell = cellIterator.next();
-                    int cellColumnIndex= cell.getColumnIndex();
-                    if(cell.getColumnIndex()<3){
-                        double numCellValue;
-                        String stringCellValue = null;
-                        switch (cell.getCellType()) {
-                            case NUMERIC:
-                                numCellValue = cell.getNumericCellValue();
-                                break;
-
-                            case STRING:
-                                stringCellValue = cell.getStringCellValue();
-                                break;
-                            default:
-                                break;
-                        }
-
-                        addWebsiteToPerson(personList, row, cellColumnIndex, stringCellValue);
-
-                    }
-                }
-                log.info("\n");
-            }
-
-            personRepository.saveAll(personList);
-        } catch (Exception e){
-            log.error(e.getMessage());
-        }
+        dataLoaderService.saveDataFromInputToDb(input);
         return "success";
     }
 
-    private void addWebsiteToPerson(List<Person> personList, XSSFRow row, int cellColumnIndex, String stringCellValue) {
-        //TODO: try to use patter matches from this link http://tutorials.jenkov.com/java-regex/pattern.html
 
-        if (cellColumnIndex%2!=0 && stringCellValue!=null && !stringCellValue.isEmpty() && stringCellValue.contains("negi")){
-            //get cell at index 0 to get website name,
-            String websiteName = row.getCell(0).getStringCellValue();
-            // match with website db if name exists in db
-            List<WebSite> webSiteList = getWebSites(websiteName);
-            //map website against person
-            Optional<Person> person = personList.stream().filter(p -> p.getLastName().toLowerCase().contains("negi")).findFirst();
-            person.ifPresent(prsn -> prsn.getRelatedWebsites().add(webSiteList.stream().findFirst().orElse(null)));
-        }
-    }
 
-    private List<WebSite> getWebSites(String websiteName) {
-        List<WebSite> webSiteList = websiteRepository.findByNameContainingIgnoreCase(websiteName);
-        //if empty, create new website
-        if (webSiteList.isEmpty()){
-            WebSite webSite = WebSite.builder().name(websiteName).url(websiteName).alias(websiteName).build();
-            websiteRepository.save(webSite);
-            webSiteList.add(webSite);
-        }
-        return webSiteList;
-    }
+
 }
--- src/main/java/com/persons/cred/services/DataLoaderService.java
+++ src/main/java/com/persons/cred/services/DataLoaderService.java
@@ -1,4 +1,96 @@
 package com.persons.cred.services;
 
+import com.persons.cred.entiities.Person;
+import com.persons.cred.entiities.WebSite;
+import com.persons.cred.repositories.PersonRepository;
+import com.persons.cred.repositories.WebsiteRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Slf4j
 public class DataLoaderService {
+    @Autowired
+    private PersonRepository personRepository;
+    @Autowired
+    private WebsiteRepository websiteRepository;
+
+    public void saveDataFromInputToDb(InputStream input) {
+        try (XSSFWorkbook workbook = new XSSFWorkbook(input)){
+            XSSFSheet spreadsheet = workbook.getSheetAt(2);
+            Iterator<Row> rowIterator = spreadsheet.iterator();
+
+            List<Person> personList = personRepository.findAll();
+
+            while (rowIterator.hasNext()) {
+                XSSFRow row = (XSSFRow) rowIterator.next();
+                //Website = storeWebsite(website cell)
+                //mapWebsiteToUser(website, usercell, passwordcell);
+
+                Iterator<Cell> cellIterator = row.cellIterator();
+                while ( cellIterator.hasNext()) {
+                    Cell cell = cellIterator.next();
+
+                    int cellColumnIndex= cell.getColumnIndex();
+                    if(cell.getColumnIndex()<3){
+                        double numCellValue;
+                        String stringCellValue = null;
+                        switch (cell.getCellType()) {
+                            case NUMERIC:
+                                numCellValue = cell.getNumericCellValue();
+                                break;
+                            case STRING:
+                                stringCellValue = cell.getStringCellValue();
+                                break;
+                            default:
+                        }
+
+                        addWebsiteToPerson(personList, row, cellColumnIndex, stringCellValue);
+
+                    }
+                }
+                log.info("\n");
+            }
+
+            personRepository.saveAll(personList);
+        } catch (Exception e){
+            log.error(e.getMessage());
+        }
+    }
+
+    public void addWebsiteToPerson(List<Person> personList, XSSFRow row, int cellColumnIndex, String stringCellValue) {
+        //TODO: try to use patter matches from this link http://tutorials.jenkov.com/java-regex/pattern.html
+        List<WebSite> webSiteList = getWebSites(row);
+        //cellColumnIndex%2!=0 means only for usernames(they are at odd columns)
+        if (cellColumnIndex%2!=0 && stringCellValue!=null && !stringCellValue.isEmpty() && stringCellValue.contains("negi")){
+            //map website against respective person
+            Optional<Person> person = personList.stream().filter(p -> p.getLastName().toLowerCase().contains("negi")).findFirst();
+            person.ifPresent(prsn -> prsn.getRelatedWebsites().add(webSiteList.stream().findFirst().orElse(null)));
+        }
+    }
+
+    public List<WebSite> getWebSites(XSSFRow row) {
+        //get cell at index 0 to get website name,
+        String websiteName = row.getCell(0).getStringCellValue();
+        // get above found website from db
+        List<WebSite> webSiteList = websiteRepository.findByNameContainingIgnoreCase(websiteName);
+        //if empty, create new website
+        if (webSiteList.isEmpty()){
+            WebSite webSite = WebSite.builder().name(websiteName).url(websiteName).alias(websiteName).build();
+            websiteRepository.save(webSite);
+            webSiteList.add(webSite);
+        }
+        return webSiteList;
+    }
 }
